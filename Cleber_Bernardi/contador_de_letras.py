f29b3f3d8ec9f54042f65a97a55d8ecf1bc967df
Cleber Bernardi <cleber62226656@alunos.sc.senac.br>
2026-06-16 20:13:17 -0300

    Comentários explicativos adicionados ao arquivo contador_de_letras.py

--- Cleber_Bernardi/contador_de_letras.py
+++ Cleber_Bernardi/contador_de_letras.py
@@ -1,11 +1,16 @@
 def contar_letras(palavra):
     resultado = {}
+
+    # Variáveis: vogais e consoantes retiradas de dentro do laço FOR para o topo da função,
+    # assim subindo o escopo para que possam ser utilizadas fora do laço FOR.
     vogais = 0
     consoantes = 0
 
     for letra in palavra:
+        # Condição para comparar se o caractere é um algarismo ou espaço.
         if letra in "0123456789" or letra == " ":
             continue
+        # Adicionadas as vogais maiúsculas para comparação.
         elif letra in "aeiouAEIOU":
             vogais = vogais + 1
         else:
