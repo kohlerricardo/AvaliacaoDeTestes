9e9fcc0e73b359136cd09c10b803aa495265e2b0
Estevão Bianchin <estevao62244656@alunos.sc.senac.br>
2026-06-16 21:06:11 -0300

    prova com comentario e atuazlização

--- Estevão_Bianchin/juros_compostos.py
+++ Estevão_Bianchin/juros_compostos.py
@@ -7,3 +7,20 @@ def simular_rendimento(capital, taxa_mensal, meses):
     montante = capital * (1 + taxa_decimal) ** meses
     
     return round(montante, 0)
+
+
+# Definir as credenciais 
+capital = float(input("Digite o capital inicial: R$ "))
+taxa_mensal = float(input("Digite a taxa de juros mensal (%): "))
+meses = int(input("Digite o tempo em meses: "))
+
+
+try: # Tentar executar o cálculo do rendimento
+
+    # Chama a função que calcula os juros compostos
+    resultado = simular_rendimento(capital, taxa_mensal, meses)
+    print(f"Montante final: R$ {resultado}")
+
+    # Captura erros do tipo ValueError gerados pela função
+except ValueError as erro:
+    print(f"Erro: {erro}") # Mensagem de erro 
