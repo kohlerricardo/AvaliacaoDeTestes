a8cb51fa7202a3291501fa13b16d344606c31763
NICOLLY FACKENBERG DE QUEIROZ <nicolly.queiroz@alunos.sc.senac.br>
2026-06-16 21:13:42 -0300

    correção do calculo de media operações

--- Nicolly_Fackenberg_de_Queiroz/calculo_media.py
+++ Nicolly_Fackenberg_de_Queiroz/calculo_media.py
@@ -1,13 +1,14 @@
 def calcular_situacao_aluno(nota1, nota2, nota3):
     if nota1 < 0 or nota2 < 0 or nota3 < 0:
         raise ValueError("Notas não podem ser negativas.")
+#media = (nota1 * 2 + nota2 + 3 + nota3 * 5) / 3 para media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
 
-    media = (nota1 * 2 + nota2 + 3 + nota3 * 5) / 3
+    media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
 
     if media >=7.0:
         return "Aprovado"
-#retirei o media <=6,9 por que todas notas abaixo de 7 presisa de recuperação.
-    elif media > 5.0:
+
+    elif media > 5.0 and media <= 6.9:
         return "Recuperação"
     else:
         return "Reprovado"
