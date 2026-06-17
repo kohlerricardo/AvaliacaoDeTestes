6c32b6c02e9951102119894f2dbf1c53932ace38
Anderson Almeida <anderson62308376@alunos.sc.senac.br>
2026-06-16 22:03:02 -0300

    Resolucao da avaliacao

--- Anderson_de_Almeida/calculo_media.py
+++ Anderson_de_Almeida/calculo_media.py
@@ -1,13 +1,13 @@
 def calcular_situacao_aluno(nota1, nota2, nota3):
-    if nota1 < 0 or nota2 < 0 or nota3 < 0:
-        raise ValueError("Notas não podem ser negativas.")
+    if not (0 <= nota1 <= 10) or not (0 <= nota2 <= 10) or not (0 <= nota3 <= 10):
+        raise ValueError("Notas devem estar entre 0 e 10")
 
-    media = (nota1 * 2 + nota2 + 3 + nota3 * 5) / 3
+    media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
 
-    if media > 7.0:
+    if media >= 7.0:
         return "Aprovado"
-
-    elif media > 5.0 and media <= 6.9:
+    elif media >= 5.0:
         return "Recuperação"
     else:
         return "Reprovado"
+        
